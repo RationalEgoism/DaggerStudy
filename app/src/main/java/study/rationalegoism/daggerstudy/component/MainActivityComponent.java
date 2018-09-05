95230b904837754fd8c62931a8aeda9d2aa51688
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-05 20:41:28 +0300

    added link to MainActivityModule from MainActivityComponent's declaration

--- app/src/main/java/study/rationalegoism/daggerstudy/component/MainActivityComponent.java
+++ app/src/main/java/study/rationalegoism/daggerstudy/component/MainActivityComponent.java
@@ -4,9 +4,10 @@ import dagger.Component;
 import study.rationalegoism.daggerstudy.adapter.RandomUserAdapter;
 import study.rationalegoism.daggerstudy.interfaces.MainActivityScope;
 import study.rationalegoism.daggerstudy.interfaces.RandomUserApi;
+import study.rationalegoism.daggerstudy.module.MainActivityModule;
 
 @MainActivityScope
-@Component(dependencies = RandomUserComponent.class)
+@Component(modules = MainActivityModule.class, dependencies = RandomUserComponent.class)
 public interface MainActivityComponent {
     RandomUserAdapter getRandomUserAdapter();
     RandomUserApi getRandomUserApi();
