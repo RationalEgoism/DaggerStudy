9c4ff8ea4e43605a41bd9c10b12d75812ad4eebe
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-05 15:58:47 +0300

    add body of RandomUserViewHolder

--- app/src/main/java/study/rationalegoism/daggerstudy/adapter/RandomUserAdapter.java
+++ app/src/main/java/study/rationalegoism/daggerstudy/adapter/RandomUserAdapter.java
@@ -4,8 +4,18 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import study.rationalegoism.daggerstudy.R;
+import study.rationalegoism.daggerstudy.model.Result;
 
 public class RandomUserAdapter extends RecyclerView.Adapter<RandomUserAdapter.RandomUserViewHolder>{
+    private List<Result> resultList = new ArrayList<>();
+
     @NonNull
     @Override
     public RandomUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -23,8 +33,13 @@ public class RandomUserAdapter extends RecyclerView.Adapter<RandomUserAdapter.Ra
     }
 
     public class RandomUserViewHolder extends RecyclerView.ViewHolder{
+        public TextView textView;
+        public ImageView imageView;
+
         public RandomUserViewHolder(View itemView) {
             super(itemView);
+            textView = itemView.findViewById(R.id.name);
+            imageView = itemView.findViewById(R.id.image);
         }
     }
 }
