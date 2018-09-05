aa34b00776840ea8caeb36220d32d2aabd83823c
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-05 15:33:57 +0300

    created body of MainActivity's onCreated()

--- app/src/main/java/study/rationalegoism/daggerstudy/MainActivity.java
+++ app/src/main/java/study/rationalegoism/daggerstudy/MainActivity.java
@@ -2,10 +2,18 @@ package study.rationalegoism.daggerstudy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import study.rationalegoism.daggerstudy.adapter.RandomUserAdapter;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +25,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+
+        Timber.plant(new Timber.DebugTree());
+
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(String message) {
+                Timber.i(message);
+            }
+        });
+
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient()
+                .newBuilder()
+                .addInterceptor(httpLoggingInterceptor)
+                .build();
+
+        retrofit = new Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://randomuser.me/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+    }
+
+
+    private void initViews() {
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
