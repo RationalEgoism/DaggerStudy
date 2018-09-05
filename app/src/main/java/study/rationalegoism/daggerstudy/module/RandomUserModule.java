4aa437cbfb615be356422ad2fd10854f7dde12a3
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-05 18:45:38 +0300

    add package for modules and create RandomUserModule

---
+++ app/src/main/java/study/rationalegoism/daggerstudy/module/RandomUserModule.java
@@ -0,0 +1,40 @@
+package study.rationalegoism.daggerstudy.module;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.squareup.picasso.Picasso;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import study.rationalegoism.daggerstudy.interfaces.RandomUserApi;
+
+@Module
+public class RandomUserModule {
+
+    @Provides
+    public RandomUserApi randomUserApi(Retrofit retrofit){
+        return retrofit.create(RandomUserApi.class);
+    }
+
+    @Provides
+    public Retrofit retrofit(OkHttpClient okHttpClient, GsonConverterFactory gsonConverterFactory){
+        return new Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://randomuser.me/")
+                .addConverterFactory(gsonConverterFactory)
+                .build();
+    }
+
+    @Provides
+    public GsonConverterFactory gsonConverterFactory(Gson gson){
+        return GsonConverterFactory.create(gson);
+    }
+
+    @Provides
+    public Gson gson(){
+        return new GsonBuilder().create();
+    }
+}
