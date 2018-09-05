37d730135f56c3f2b3ded86e2810fa317b057b5c
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-05 19:08:00 +0300

    added includes for modules

--- app/src/main/java/study/rationalegoism/daggerstudy/module/RandomUserModule.java
+++ app/src/main/java/study/rationalegoism/daggerstudy/module/RandomUserModule.java
@@ -11,7 +11,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import study.rationalegoism.daggerstudy.interfaces.RandomUserApi;
 
-@Module
+@Module(includes = OkHttpClientModule.class)
 public class RandomUserModule {
 
     @Provides
