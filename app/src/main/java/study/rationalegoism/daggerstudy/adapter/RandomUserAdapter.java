18bb1bd7cd3c5f6e6bec220d55d7300ff6b74768
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-05 16:04:46 +0300

    add body of RandomUserAdapter

--- app/src/main/java/study/rationalegoism/daggerstudy/adapter/RandomUserAdapter.java
+++ app/src/main/java/study/rationalegoism/daggerstudy/adapter/RandomUserAdapter.java
@@ -2,10 +2,13 @@ package study.rationalegoism.daggerstudy.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,17 +22,24 @@ public class RandomUserAdapter extends RecyclerView.Adapter<RandomUserAdapter.Ra
     @NonNull
     @Override
     public RandomUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_random_user,
+                parent, false);
+        return new RandomUserViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RandomUserViewHolder holder, int position) {
-
+        Result result = resultList.get(position);
+        holder.textView.setText(String.format("%s %s", result.getName().getFirst(),
+                result.getName().getLast()));
+        Picasso.with(holder.imageView.getContext())
+                .load(result.getPicture().getLarge())
+                .into(holder.imageView);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return resultList.size();
     }
 
     public class RandomUserViewHolder extends RecyclerView.ViewHolder{
