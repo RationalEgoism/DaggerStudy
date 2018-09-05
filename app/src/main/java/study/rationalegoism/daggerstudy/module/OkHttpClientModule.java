de3b609623d761025e5bdf2064063f1abc1801dc
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-05 18:57:06 +0300

    create OkHttpClientModule

---
+++ app/src/main/java/study/rationalegoism/daggerstudy/module/OkHttpClientModule.java
@@ -0,0 +1,50 @@
+package study.rationalegoism.daggerstudy.module;
+
+import android.content.Context;
+
+import java.io.File;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import timber.log.Timber;
+
+@Module
+public class OkHttpClientModule {
+
+    @Provides
+    public OkHttpClient okHttpClient(Cache cache, HttpLoggingInterceptor httpLoggingInterceptor){
+        return new OkHttpClient().newBuilder()
+                .cache(cache)
+                .addInterceptor(httpLoggingInterceptor)
+                .build();
+    }
+
+    @Provides
+    public Cache cache(File cacheFile){
+        return new Cache(cacheFile, 10 * 1000 * 1000); // 10MB
+    }
+
+    @Provides
+    public HttpLoggingInterceptor HttpLoggingInterceptor(){
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(String message) {
+                Timber.i(message);
+            }
+        });
+
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return httpLoggingInterceptor;
+    }
+
+    @Provides
+    public File file(Context context){
+        File file = new File(context.getCacheDir(), "HttpCache");
+        file.mkdirs();
+        return file;
+    }
+
+}
