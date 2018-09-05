fbeedaf008974ee529eb0fb6aef3a12ab2644027
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-05 19:33:32 +0300

    added our own annotation RandomUserApplicationScope

---
+++ app/src/main/java/study/rationalegoism/daggerstudy/interfaces/RandomUserApplicationScope.java
@@ -0,0 +1,11 @@
+package study.rationalegoism.daggerstudy.interfaces;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Retention(RetentionPolicy.CLASS)
+public @interface RandomUserApplicationScope {
+}
