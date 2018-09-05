253eabf30eef0169bb9da5572e71ea63cc7737ed
StanislavTalanov <rationalegoism1470@gmail.com>
2018-09-05 20:58:26 +0300

    updated MainActivity with MainActivityComponent

--- app/src/main/java/study/rationalegoism/daggerstudy/MainActivity.java
+++ app/src/main/java/study/rationalegoism/daggerstudy/MainActivity.java
@@ -10,11 +10,12 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import study.rationalegoism.daggerstudy.adapter.RandomUserAdapter;
-import study.rationalegoism.daggerstudy.component.DaggerRandomUserComponent;
-import study.rationalegoism.daggerstudy.component.RandomUserComponent;
+import study.rationalegoism.daggerstudy.application.RandomUserApplication;
+import study.rationalegoism.daggerstudy.component.DaggerMainActivityComponent;
+import study.rationalegoism.daggerstudy.component.MainActivityComponent;
 import study.rationalegoism.daggerstudy.interfaces.RandomUserApi;
 import study.rationalegoism.daggerstudy.model.RandomUsers;
-import study.rationalegoism.daggerstudy.module.ContextModule;
+import study.rationalegoism.daggerstudy.module.MainActivityModule;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,24 +24,23 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     RandomUserAdapter mAdapter;
 
-    Picasso picasso;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
-
-        Timber.plant(new Timber.DebugTree());
-
-        RandomUserComponent component = DaggerRandomUserComponent.builder()
-                .contextModule(new ContextModule(this))
-                .build();
-
-        picasso = component.getPicasso();
-        randomUserApi = component.getRandomUserApi();
+        afterActivityLevelComponent();
         populateUsers();
 
+    }
+
+    private void afterActivityLevelComponent() {
+        MainActivityComponent component = DaggerMainActivityComponent.builder()
+                .mainActivityModule(new MainActivityModule(this))
+                .randomUserComponent(RandomUserApplication.get(this).getRandomUserApplicationComponent())
+                .build();
+        randomUserApi = component.getRandomUserApi();
+        mAdapter = component.getRandomUserAdapter();
     }
 
     private void populateUsers() {
@@ -49,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<RandomUsers> call, @NonNull Response<RandomUsers> response) {
                 if(response.isSuccessful()) {
-                    mAdapter = new RandomUserAdapter();
                     mAdapter.setItems(response.body().getResults());
                     recyclerView.setAdapter(mAdapter);
                 }
